f35e634c83f15b7e955f9bd923512b1ad381cd46
Ben Lorentz <blorentz@luc.edu>
2019-03-19 18:54:01 -0500

    need to finish routs.py

--- flasktut/app/routes.py
+++ flasktut/app/routes.py
@@ -1,7 +1,13 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form)
