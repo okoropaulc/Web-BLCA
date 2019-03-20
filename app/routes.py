3cc1f257e8ca2bfda6523c26b17ebfdac3294ba5
okoropaulc@yahoo.com <okoropaulc@yahoo.com>
2019-03-19 23:25:08 -0500

    add html to the python code

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Paul'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - BLCA</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
